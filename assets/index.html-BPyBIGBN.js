874068a8a6716d2961860e73301e7cfac57f21ec
Mister-Hope <mister-hope@outlook.com>
2024-02-20 02:59:33 +0000

    Deploying to gh-pages from @ vuepress/ecosystem@3a6e00bd1e63ab69485ea996c603bb802d8baaa1 🚀

---
+++ assets/index.html-BPyBIGBN.js
@@ -0,0 +1,1 @@
+import{_ as t,r as o,o as a,c as n,a as l}from"./app-BcH8wZQx.js";const s={};function c(r,_){const e=o("Catalog");return a(),n("div",null,[l(e)])}const m=t(s,[["render",c],["__file","index.html.vue"]]),p=JSON.parse('{"path":"/tools/","title":"Tools","lang":"en-US","frontmatter":{"title":"Tools"},"headers":[],"git":{},"filePathRelative":null}');export{m as comp,p as data};
